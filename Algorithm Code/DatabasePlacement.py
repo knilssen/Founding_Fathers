2b1fa897998ab1ba8b5d5fe4bfe366af379f188c
Kristian Nilssen <kcn0519@westminstercollege.edu>
2017-03-30 02:51:57 -0600

    Database Placements Additions and Updates
    Database Placements Additions and Updates

--- Algorithm Code/DatabasePlacement.py
+++ Algorithm Code/DatabasePlacement.py
@@ -11,6 +11,7 @@ Usage:
 
 import sys
 import personOfInterestWeightWithInputs
+import article_Grabber
 import time
 import threading
 import /article_grabbers/grabber_ksl
@@ -44,7 +45,7 @@ def grabber_worker(source_name, source, currentTime, allkeywords, DiffName):
     t = threading.currentThread()
     art = source.main(currentTime)
     for url in art:
-        print (url)
+        # print (url)
         a = threading.Thread(target=article_worker, args=(url, art[url], source_name, allkeywords, DiffName))
         thread.append(a)
         a.start()
