9ff30e82c7b03597b02d2692f804261fe1e6593c
Kristian Nilssen <kcn0519@westminstercollege.edu>
2017-04-04 10:58:14 -0600

    Update database_placement.py

--- Algorithm Code/database_placement.py
+++ Algorithm Code/database_placement.py
@@ -13,15 +13,15 @@ import sys
 import article_NERT_parser
 import time
 import threading
-import /article_grabbers/grabber_ksl
-import /article_grabbers/grabber_deseret_news
-import /article_grabbers/grabber_fox13
-import /article_grabbers/grabber_slt
-import /article_grabbers/grabber_utah_policy
-import /article_grabbers/grabber_upc_legislative
-import /article_grabbers/grabber_upc_judicial
-import /article_grabbers/grabber_upc_flagged_bill_status
-import /article_grabbers/grabber_upc_executive
+import article_grabbers/grabber_ksl
+import article_grabbers/grabber_deseret_news
+import article_grabbers/grabber_fox13
+import article_grabbers/grabber_slt
+import article_grabbers/grabber_utah_policy
+import article_grabbers/grabber_upc_legislative
+import article_grabbers/grabber_upc_judicial
+import article_grabbers/grabber_upc_flagged_bill_status
+import article_grabbers/grabber_upc_executive
 import multiprocessing
 
 maxthreads = multiprocessing.cpu_count()
