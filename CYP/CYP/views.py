400a8aa5c40133999d88260792321db6175935ac
Kristian Nilssen <kcn0519@westminstercollege.edu>
2017-04-28 02:39:59 -0600

    Database size fix
    Fix for if the article pool is less than 9

--- CYP/CYP/views.py
+++ CYP/CYP/views.py
@@ -16,50 +16,41 @@ def aboutUs (request):
 
 def news_scroll_one(request):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT url, source, post_date, title FROM cyp.News_articles JOIN cyp.News_article_person on cyp.News_articles.id = cyp.News_article_person.articles_id_id WHERE cyp.News_article_person.people_id_id = 110 ORDER BY cyp.News_article_person.weight DESC LIMIT 9")
+        cursor.execute("SELECT url, source, post_date, title FROM cyp.News_articles JOIN cyp.News_article_person on cyp.News_articles.id = cyp.News_article_person.articles_id_id WHERE cyp.News_article_person.people_id_id = 110 ORDER BY cyp.News_article_person.weight DESC")
         scrolls = cursor.fetchall()
         scroll=[{},{},{},{},{},{},{},{},{}]
-        scroll[0].update({'url': scrolls[0][0], 'source': scrolls[0][1], 'post_date': scrolls[0][2], 'title': scrolls[0][3]})
-        scroll[1].update({'url': scrolls[1][0], 'source': scrolls[1][1], 'post_date': scrolls[1][2], 'title': scrolls[1][3]})
-        scroll[2].update({'url': scrolls[2][0], 'source': scrolls[2][1], 'post_date': scrolls[2][2], 'title': scrolls[2][3]})
-        scroll[3].update({'url': scrolls[3][0], 'source': scrolls[3][1], 'post_date': scrolls[3][2], 'title': scrolls[3][3]})
-        scroll[4].update({'url': scrolls[4][0], 'source': scrolls[4][1], 'post_date': scrolls[4][2], 'title': scrolls[4][3]})
-        scroll[5].update({'url': scrolls[5][0], 'source': scrolls[5][1], 'post_date': scrolls[5][2], 'title': scrolls[5][3]})
-        scroll[6].update({'url': scrolls[6][0], 'source': scrolls[6][1], 'post_date': scrolls[6][2], 'title': scrolls[6][3]})
-        scroll[7].update({'url': scrolls[7][0], 'source': scrolls[7][1], 'post_date': scrolls[7][2], 'title': scrolls[7][3]})
-        scroll[8].update({'url': scrolls[8][0], 'source': scrolls[8][1], 'post_date': scrolls[8][2], 'title': scrolls[8][3]})
+        if len(scrolls) < 9:
+            article_pool_size = len(scrolls)
+        else:
+            article_pool_size = 9
+        for x in range(0,article_pool_size):
+            scroll[x].update({'url': scrolls[x][0], 'source': scrolls[x][1], 'post_date': scrolls[x][2], 'title': scrolls[x][3]})
     return render_to_response('News/news_scroll_one.html', {'scrolls': scroll})
 
 def news_scroll_two(request):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT url, source, post_date, title FROM cyp.News_articles JOIN cyp.News_article_person on cyp.News_articles.id = cyp.News_article_person.articles_id_id WHERE cyp.News_article_person.people_id_id = 105 ORDER BY cyp.News_article_person.weight DESC LIMIT 9")
+        cursor.execute("SELECT url, source, post_date, title FROM cyp.News_articles JOIN cyp.News_article_person on cyp.News_articles.id = cyp.News_article_person.articles_id_id WHERE cyp.News_article_person.people_id_id = 105 ORDER BY cyp.News_article_person.weight DESC")
         scrolls = cursor.fetchall()
         scroll=[{},{},{},{},{},{},{},{},{}]
-        scroll[0].update({'url': scrolls[0][0], 'source': scrolls[0][1], 'post_date': scrolls[0][2], 'title': scrolls[0][3]})
-        scroll[1].update({'url': scrolls[1][0], 'source': scrolls[1][1], 'post_date': scrolls[1][2], 'title': scrolls[1][3]})
-        scroll[2].update({'url': scrolls[2][0], 'source': scrolls[2][1], 'post_date': scrolls[2][2], 'title': scrolls[2][3]})
-        scroll[3].update({'url': scrolls[3][0], 'source': scrolls[3][1], 'post_date': scrolls[3][2], 'title': scrolls[3][3]})
-        scroll[4].update({'url': scrolls[4][0], 'source': scrolls[4][1], 'post_date': scrolls[4][2], 'title': scrolls[4][3]})
-        scroll[5].update({'url': scrolls[5][0], 'source': scrolls[5][1], 'post_date': scrolls[5][2], 'title': scrolls[5][3]})
-        scroll[6].update({'url': scrolls[6][0], 'source': scrolls[6][1], 'post_date': scrolls[6][2], 'title': scrolls[6][3]})
-        scroll[7].update({'url': scrolls[7][0], 'source': scrolls[7][1], 'post_date': scrolls[7][2], 'title': scrolls[7][3]})
-        scroll[8].update({'url': scrolls[8][0], 'source': scrolls[8][1], 'post_date': scrolls[8][2], 'title': scrolls[8][3]})
+        if len(scrolls) < 9:
+            article_pool_size = len(scrolls)
+        else:
+            article_pool_size = 9
+        for x in range(0,article_pool_size):
+            scroll[x].update({'url': scrolls[x][0], 'source': scrolls[x][1], 'post_date': scrolls[x][2], 'title': scrolls[x][3]})
     return render_to_response('News/news_scroll_two.html', {'scrolls': scroll})
 
 def news_scroll_three(request):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT url, source, post_date, title FROM cyp.News_articles JOIN cyp.News_article_person on cyp.News_articles.id = cyp.News_article_person.articles_id_id WHERE cyp.News_article_person.people_id_id = 106 ORDER BY cyp.News_article_person.weight DESC LIMIT 9")
+        cursor.execute("SELECT url, source, post_date, title FROM cyp.News_articles JOIN cyp.News_article_person on cyp.News_articles.id = cyp.News_article_person.articles_id_id WHERE cyp.News_article_person.people_id_id = 106 ORDER BY cyp.News_article_person.weight DESC")
         scrolls = cursor.fetchall()
         scroll=[{},{},{},{},{},{},{},{},{}]
-        scroll[0].update({'url': scrolls[0][0], 'source': scrolls[0][1], 'post_date': scrolls[0][2], 'title': scrolls[0][3]})
-        scroll[1].update({'url': scrolls[1][0], 'source': scrolls[1][1], 'post_date': scrolls[1][2], 'title': scrolls[1][3]})
-        scroll[2].update({'url': scrolls[2][0], 'source': scrolls[2][1], 'post_date': scrolls[2][2], 'title': scrolls[2][3]})
-        scroll[3].update({'url': scrolls[3][0], 'source': scrolls[3][1], 'post_date': scrolls[3][2], 'title': scrolls[3][3]})
-        scroll[4].update({'url': scrolls[4][0], 'source': scrolls[4][1], 'post_date': scrolls[4][2], 'title': scrolls[4][3]})
-        scroll[5].update({'url': scrolls[5][0], 'source': scrolls[5][1], 'post_date': scrolls[5][2], 'title': scrolls[5][3]})
-        scroll[6].update({'url': scrolls[6][0], 'source': scrolls[6][1], 'post_date': scrolls[6][2], 'title': scrolls[6][3]})
-        scroll[7].update({'url': scrolls[7][0], 'source': scrolls[7][1], 'post_date': scrolls[7][2], 'title': scrolls[7][3]})
-        scroll[8].update({'url': scrolls[8][0], 'source': scrolls[8][1], 'post_date': scrolls[8][2], 'title': scrolls[8][3]})
+        if len(scrolls) < 9:
+            article_pool_size = len(scrolls)
+        else:
+            article_pool_size = 9
+        for x in range(0,article_pool_size):
+            scroll[x].update({'url': scrolls[x][0], 'source': scrolls[x][1], 'post_date': scrolls[x][2], 'title': scrolls[x][3]})
     return render_to_response('News/news_scroll_three.html', {'scrolls': scroll})
 
 
